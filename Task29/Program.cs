83487bed710e97b391a802c6f0b458c46bc11dd7
Peshkov Dmitry <peshkov-dv@yandex.ru>
2023-03-18 16:09:16 +0300

    Задача 29. Количество вводимых элементов - 8. Не совсем понятно, по условию в  массиве 8 ячеек, а в тестовых ответах в одном случае пять, а в другом - три

---
+++ Task29/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 29. Напишите программу, которая будет создавать массив из 8 символов
+// и заполнять значениями от пользователя
+void GetArray(int[] array) {
+    for (int i = 0; i < array.Length; i++) {
+        Console.Write($"Введите элемент {i}: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+Console.Clear();
+int[] array = new int[8];
+GetArray(array);
+Console.WriteLine($"[{String.Join(", ",array)}]");
