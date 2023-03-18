a2a68135e93188bcdfc8b4b85582bcfd5f85edb6
Peshkov Dmitry <peshkov-dv@yandex.ru>
2023-03-18 15:41:38 +0300

    Задача 25

---
+++ Task25/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 25. Напишите цикл, который принимает на вход два числа (А и В) 
+// и возводит число А в натуральную степень В
+int Pow(int x, int y) {
+    int res = 1;
+    for(int i = 0; i < y; i++) {
+        res *= x;
+    }
+    return res;
+}
+
+Console.Clear();
+Console.Write("Введите А:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите В:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{a}, {b} -> {Pow(a,b)}");
