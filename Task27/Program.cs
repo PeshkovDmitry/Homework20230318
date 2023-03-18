1885ee5e7aaeed4efc4e7fc224929a73fe2c4740
Peshkov Dmitry <peshkov-dv@yandex.ru>
2023-03-18 15:48:22 +0300

    Задача 27

---
+++ Task27/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 27. Напишите программу, который принимает на вход 
+// число и выдает сумму цифр в числе
+int Sum(int x) {
+    int res = 0;
+    if (x>0) {
+        while (x > 0) {
+            res += x%10;
+            x /= 10;
+        }
+    } 
+    return res;
+}
+Console.Clear();
+Console.Write("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{n} -> {Sum(n)}");
